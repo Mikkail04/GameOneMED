ee59165ae2c315a92ab2271e330e4b3269c316be
EricSal2004 <Ericsal121@gmail.com>
2023-03-29 09:24:27 -0400

    Added a button and interface

--- GameGUI.java
+++ GameGUI.java
@@ -1,7 +1,32 @@
+import java.awt.Dimension;
+import java.awt.Rectangle;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GameGUI extends JFrame{
     public GameGUI(){
-        
+        JFrame start_frame = new JFrame("P.D");
+        int width = 1200;
+        int height = 800;
+        start_frame.setSize(width, height);
+        start_frame.setVisible(true);
+        start_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Panel to hold our buttons
+        JPanel start_panel = new JPanel();
+        start_panel.setLayout(null);
+        start_frame.add(start_panel);
+
+
+        // Button to initialize everything
+        JButton start_button = new JButton("Start");
+        int buttonWidth = 80;
+        int buttonHeight = 20;
+        start_button.setBounds(new Rectangle((width - buttonWidth)/2, (height - buttonHeight)/2, buttonWidth, buttonHeight));
+        start_button.setSize(new Dimension(buttonWidth, buttonHeight));
+        start_button.setFocusable(false);
+        start_panel.add(start_button);
     }
 }
