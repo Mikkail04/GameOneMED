e22c9d1e4038e34a95fa4830f4d2ae52540e97de
Denny <dennyrfernandez2004@gmail.com>
2023-03-29 23:59:54 -0400

    Made Scenario class

---
+++ scenario.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+public class scenario {
+    
+    private String scenarioName;
+    ArrayList<String> options = new ArrayList<String>();;
+    private String backgroundImage;
+
+    /**
+     * @param name sets name of scenario
+     */
+    public scenario(String name){
+        this.scenarioName = name;
+    }
+
+    /**
+     * @param option adds an option to choose from to options list
+     */
+    public void addOptions(String option){
+        this.options.add(option);
+    }
+}
