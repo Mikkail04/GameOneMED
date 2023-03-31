071e9495daa1470f8d2497da75db2addf5bd4575
Denny <dennyrfernandez2004@gmail.com>
2023-03-30 21:52:20 -0400

    Made a player class

---
+++ player.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class player {   
+    private String name;
+    private int lvl;
+    private int hp;
+    private int atk;
+    private ArrayList<String> inv;
+    Random rand = new Random();
+
+    /**
+     * @param name When first starting the game, contruct the object while asking the user to enter a name
+     * hp and atk starting stats are TBD, give them actual numbers when doing combat
+     */
+    public player(String name){
+        this.name = name;
+        hp = 10;
+        atk = 5;
+        lvl = 1;
+        inv = new ArrayList<String>();
+    }
+
+    /**
+     * When leveling up, stats will decrease randomly
+     * Decide the range when working on combat
+     */
+    public void levelUp(){
+        lvl++;
+        hp += rand.nextInt(1, 5);
+        atk += rand.nextInt(1, 3);
+    }
+}
