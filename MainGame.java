b131acefd8d76388f51a6f3f3b62106518e2733f
EricSal2004 <Ericsal121@gmail.com>
2023-11-11 21:55:27 -0500

    I changed how the Damage text looks

--- MainGame.java
+++ MainGame.java
@@ -2,7 +2,7 @@ public class MainGame {
     public static void main(String[] args) {
         // GameGUI game = new GameGUI(); 
         Character hero = new Character(100,20, "Denny");
-        Monster villan = new Monster(120,20,"daniel");
+        Monster villan = new Monster(120,20,"Daniel");
         hero.attack(villan);
     }
 }
