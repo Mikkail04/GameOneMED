4e6e76c9303161311b71630d337df7c19cc9e289
Denny Fernandez <dennyrfernandez2004@gmail.com>
2023-11-12 00:25:28 -0500

    Monster no longer attacks after death.

--- MainGame.java
+++ MainGame.java
@@ -10,10 +10,8 @@ public class MainGame {
         System.out.println("-----------------PHANTHOM VEIL-----------------");
         System.out.println("\nBrave Warrior what is your name?\n");
         Scanner scan = new Scanner(System.in);
-
         String player = scan.nextLine();
-
-        Character mainCharacter = new Character(1,10,player);
+        Character mainCharacter = new Character(100,10,player);
         Monster goblin = new Monster(50,5,"Goblin");
 
         System.out.println("Welcome Young Blood, for your first Task I want you to defeat that goblin over there\n");
@@ -26,28 +24,34 @@ public class MainGame {
         int turnCount = 1;
         Scanner scan = new Scanner(System.in);
 
+        //Goes until one gets JFkd
         while(mc.getHealth() != 0 && mon.getHealth() != 0){
             System.out.println("\n\nTurn: " + turnCount);
-            System.out.println("Pick a move lil bro Your options are:\\n" + //
-                    "Attack\\n" + //
-                    "Defend\\n");  
-            String opt = scan.nextLine().toLowerCase();
+            System.out.println("Pick a move lil bro Your options are:\n" + //
+                    "1: Attack\n" + //
+                    "2: Defend\n");  
+            //Scanne the playere
+            String opt = scan.nextLine();
             switch(opt){
-                case "attack":
+                //Attake the mon
+                case "1":
                 mc.attack(mon);
-                mon.attack(mc);
-                    break;
-                case "defend":
+                if(mon.getHealth() > 0)
+                    mon.attack(mc);
+                break;
+                //Defende da mon
+                case "2":
                     mon.weakAttack(mc);
                     break;
-                case "sexo??":
+                //SEXO????????????????????????
+                case "SEXO??":
                     System.out.println("Ur attack got a small buff");
-
                     mc.setAttackPower(9999999);
                 default:
                     System.out.println("Enter something stupid");
             }
             turnCount++;
         }
+        scan.close();
     }
 }
