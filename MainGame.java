bbcb7b145dc205ab1c713dec3fdb95e38e0d0c87
EricSal2004 <Ericsal121@gmail.com>
2023-11-11 22:23:09 -0500

    Testing

--- MainGame.java
+++ MainGame.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class MainGame {
     public static void main(String[] args) {
         // GameGUI game = new GameGUI(); 
-        Character hero = new Character(100,20, "Denny");
-        Monster villan = new Monster(120,20,"Daniel");
-        hero.attack(villan);
+        Game();
+    }
+    public static void Game(){
+        System.out.println("-----------------PHANTHOM VEIL-----------------");
+        System.out.println("\nBrave Warrior what is your name?\n");
+
+        Scanner userInput = new Scanner(System.in);
+        String player = userInput.nextLine();
+
+        Character mainCharacter = new Character(100,10,player);
+        Monster goblin = new Monster(50,5,"Goblin");
+
+        System.out.println("Welcome Young Blood, for your first Task I want you to defeat that goblin over there\n");
+
+        mainCharacter.attack(goblin);
     }
 }
