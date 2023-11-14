c9560c2d925552926ac91c969d4eacaabf53629b
M <dgood@LAPTOP-KF0CNE15.(none)>
2023-11-13 22:08:54 -0500

    - Adds intro text when an enemy appears - Adds 'hobgoblin' enemy that has a 30% chance to spawn before closing the game

--- MainGame.java
+++ MainGame.java
@@ -12,7 +12,8 @@ public class MainGame {
         String player = scan.nextLine();
         Character mainCharacter = new Character(100,10,player); // Player
         Monster goblin = new Monster(50,5,"Goblin");
-        Monster goblinTwo = new Monster(50,5,"TestDummy");
+        Monster goblinTwo = new Monster(50,5,"GoblinTwo");
+        Monster hobgoblin = new Monster(75,10,"Hobgoblin");
 
         System.out.println("Welcome " + player + ", you shall be the defender of Earth. For your first task, you should defeat that goblin over there\n");
 
@@ -20,10 +21,24 @@ public class MainGame {
         Item potion = new Heals(10, "Healing Potion"); /* This is just testing the inventory system */
         System.out.println("Here is a potion for your hard fought victory!");
         mainCharacter.addToInventory(potion); // Ngl idk why I needed to do mainCharacter first for the methods to work
-        mainCharacter.addToInventory(potion); // Testing to see if I can have multiple items
+        //mainCharacter.addToInventory(potion); // Testing to see if I can have multiple items
         mainCharacter.showInventory(); // Testing the showing inventory systemmaBobber
-
-        Fight.vs(mainCharacter,goblinTwo, scan);
+        
+        // The player now receives a potion from fighting the second goblin
+        System.out.println("\n\n\nAnother goblin approaches!");
+        Fight.vs(mainCharacter, goblinTwo, scan);
+        System.out.println("Here is a potion for your hard fought victory!");
+        mainCharacter.addToInventory(potion);
+        
+        // Chance for a hobgoblin to spawn that gives the player two potions
+        double hobGobChance = Math.random() * 100;
+        if (hobGobChance <= 30 ){
+        System.out.println("\n\n\nA hobgoblin approaches!");
+        Fight.vs(mainCharacter,hobgoblin, scan);
+        System.out.println("Here are two potions for your hard fought victory!");
+        mainCharacter.addToInventory(potion); 
+        mainCharacter.addToInventory(potion);
+        }
         scan.close();
     }
 
